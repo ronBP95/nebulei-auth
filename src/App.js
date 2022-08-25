78cdf9e3e83f12837e374d17a4e1a553abfedaff
Adrian Blanc <adrian@adrianblanc.live>
2022-08-25 12:15:32 -0500

    deletion of home

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Route, Routes } from "react-router-dom"
 
-import Home from './containers/Home/index'
 import Register from './containers/Register/index'
 import Login from './containers/Login/index'
 
@@ -10,8 +9,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Register />} />
+        <Route path="/" element={<Register />} />
         <Route path="/login" element={<Login />} />
       </Routes>
     </>
